f4b0a56b3908590cd88401a80362a0f387cf7c25
KalvinKouch <kskouch@uci.edu>
2024-02-12 17:28:15 -0800

    Added comments for clarity

--- Nasa-Web-Game/Assets/Scripts/PlayerMovement.cs
+++ Nasa-Web-Game/Assets/Scripts/PlayerMovement.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    
     public float horizontal;
     private float speed = 8f;
     private float jumpingPower = 8f;
@@ -12,6 +13,7 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         horizontal = Input.GetAxisRaw("Horizontal");
+        //Gets User input and checks to see if the User can Jump
         if(Input.GetButtonDown("Jump") && !canJump)
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
@@ -22,10 +24,12 @@ public class PlayerMovement : MonoBehaviour
     }
     private void FixedUpdate()
     {
+        //Movement speed of sprite
         rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
     }
     private void OnCollisionEnter2D(Collision2D ground)
-    {
+    {   
+        //checks to see if sprite is on the tag "Ground"
         if(ground.gameObject.CompareTag("Ground"))
         {
             canJump = false;
