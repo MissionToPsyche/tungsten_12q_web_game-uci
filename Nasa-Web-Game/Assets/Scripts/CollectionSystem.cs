701b36b2ce5382f21bb1db8a220dcda17f8676f4
KalvinKouch <kskouch@uci.edu>
2024-02-12 20:47:52 -0800

    Displays the count of the items collected

--- Nasa-Web-Game/Assets/Scripts/CollectionSystem.cs
+++ Nasa-Web-Game/Assets/Scripts/CollectionSystem.cs
@@ -1,19 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System;
 
 public class CollectionSystem : MonoBehaviour
 {
-
+    public TMP_Text collectedAmount;
+    private int counter;
     // Update is called once per frame
     void Update()
     {
-        
+        collectedAmount.text = counter.ToString();
     }
     private void OnTriggerEnter2D(Collider2D item)
     {
         if (item.gameObject.CompareTag("Item1"))
         {
+            counter++;
             Destroy(item.gameObject);
         }
     }
