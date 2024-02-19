347f594437ae4c0b1c5165ed471ad509ccb0fdbb
KalvinKouch <kskouch@uci.edu>
2024-02-18 20:29:27 -0800

    Implemented Jump Buff

--- Nasa-Web-Game/Assets/Scripts/PlayerMovement.cs
+++ Nasa-Web-Game/Assets/Scripts/PlayerMovement.cs
@@ -9,9 +9,13 @@ public class PlayerMovement : MonoBehaviour
     private float speed = 8f;
     private float jumpingPower = 8f;
     public Rigidbody2D rb;
+    //checks to see if user can jump
     private bool canJump;
+    //checks to see if user has speed buff
     private bool speedBuff = false;
     private float speedTimer = 10;
+    private bool jumpBuff = false;
+    private float jumpTimer = 10;
     void Update()
     {
         horizontal = Input.GetAxisRaw("Horizontal");
@@ -34,25 +38,45 @@ public class PlayerMovement : MonoBehaviour
 
             }
         }
+        if (jumpBuff)
+        {
+            //timer for jump buff
+            jumpTimer -= Time.deltaTime;
+            if (jumpTimer < 3)
+            {
+                jumpingPower = 8f;
+                jumpTimer = 10;
+                jumpBuff = false;
 
-        
+            }
+        }
+
+
     }
     private void FixedUpdate()
     {
         //Movement speed of sprite
         rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
     }
-    private void OnTriggerEnter2D(Collider2D speedbuff)
+    private void OnTriggerEnter2D(Collider2D buff)
     {
         //if colliding with speedbuff then it speedbuff = true
-        if(speedbuff.gameObject.CompareTag("SpeedBuff"))
+        if(buff.gameObject.CompareTag("Speed Buff"))
         {
             speedBuff = true;
             speed = 12f;
 
-            Destroy(speedbuff.gameObject);
+            Destroy(buff.gameObject);
+        }
+        if (buff.gameObject.CompareTag("Jump Buff"))
+        {
+            jumpBuff = true;
+            jumpingPower = 12f;
+
+            Destroy(buff.gameObject);
         }
     }
+ 
     private void OnCollisionEnter2D(Collision2D ground)
     {   
         //checks to see if sprite is on the tag "Ground"
