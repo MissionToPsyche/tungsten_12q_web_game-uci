79824c3dc9adfe7c3c52f20a57ca5376e85ab4bc
KalvinKouch <kskouch@uci.edu>
2024-03-04 21:11:49 -0800

    Fixed Parallax Background
    Now the background is moving as intended

--- Nasa-Web-Game/Assets/Scripts/Parallax/Parallax.cs
+++ Nasa-Web-Game/Assets/Scripts/Parallax/Parallax.cs
@@ -5,19 +5,20 @@ using UnityEngine;
 public class Parallax : MonoBehaviour
 {
     private float length, start;
-    public GameObject camera;
+    private GameObject camera;
     public float parallaxEffect;
     // Start is called before the first frame update
     void Start()
     {
+        camera = GameObject.Find("Virtual Camera");
         start = transform.position.x;
         length = GetComponent<SpriteRenderer>().bounds.size.x;
     }
 
     // Update is called once per frame
-    void fixedUpdate()
+    void Update()
     {
         float distance = (camera.transform.position.x * parallaxEffect);
-        transform.position = new Vector3(0, 0, transform.position.z);
+        transform.position = new Vector3(start + distance, transform.position.y, transform.position.z);
     }
 }
