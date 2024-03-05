3f8b654eec60e8be8b3d7631d08604cd6e3c686b
KalvinKouch <kskouch@uci.edu>
2024-03-05 12:08:22 -0800

    Level 1 Design Progress

--- Nasa-Web-Game/Assets/Scripts/Parallax/Parallax.cs
+++ Nasa-Web-Game/Assets/Scripts/Parallax/Parallax.cs
@@ -5,18 +5,17 @@ using UnityEngine;
 public class Parallax : MonoBehaviour
 {
     private float length, start;
-    private GameObject camera;
+    public GameObject camera;
     public float parallaxEffect;
     // Start is called before the first frame update
     void Start()
     {
-        camera = GameObject.Find("Virtual Camera");
         start = transform.position.x;
         length = GetComponent<SpriteRenderer>().bounds.size.x;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         float distance = (camera.transform.position.x * parallaxEffect);
         transform.position = new Vector3(start + distance, transform.position.y, transform.position.z);
